97b0193f483f14171e0ffe5a3b3caeb67b034e31
güleser kırımoğlu <guleserkirimoglu1495@gmail.com>
2021-02-28 15:35:40 +0300

    Add project files.

---
+++ RealMyDictionary/Dictionary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealMyDictionary
+{
+    class Dictionary
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ RealMyDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RealMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+
+            //Dictionary dictionary1 = new Dictionary();
+            //dictionary1.CustomerId = 1;
+            //dictionary1.Name = "mustafa";
+            //dictionary1.LastName = "Kutlu";
+
+            //Dictionary dictionary2 = new Dictionary();
+            //dictionary2.CustomerId = 2;
+            //dictionary2.Name = "Cemil";
+            //dictionary2.LastName = "Meriç";
+
+            //Dictionary dictionary3 = new Dictionary();
+            //dictionary3.CustomerId = 3;
+            //dictionary3.Name = "Ahmet h.";
+            //dictionary3.LastName = "Tanpınar";
+
+            //MyDictionary<string> isimler = new MyDictionary<string> ();
+
+
+            isimler.Add("cahit zarifoğlu");
+            isimler.Add("Kemal Özer");
+            
+
+        }
+    }
+}
